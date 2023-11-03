75c5b03bfeac44d482397d4fd69e23227ee9bef3
thsator <62711665+thsator@users.noreply.github.com>
2023-11-03 11:11:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Set the target date and time for the countdown (YYYY-MM-DD HH:MM:SS format)
+const targetDate = new Date("2023-10-21T14:59:59").getTime();
+
+// Update the countdown every second
+const timer = setInterval(function () {
+    const currentDate = new Date().getTime();
+    const timeLeft = targetDate - currentDate;
+
+    if (timeLeft <= 0) {
+        clearInterval(timer);
+        document.getElementById("countdown").innerHTML = "Time's up!";
+    } else {
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        const countdownText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        document.getElementById("countdown").innerHTML = countdownText;
+    }
+}, 1000);
